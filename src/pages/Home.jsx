42f5210e5399be17664880dd088da79c4fc87133
Inderveer Johal <Inderveerjohal0@gmail.com>
2025-03-10 15:05:41 -0700

    feat: added link to Home.jsx
    feat: added basic layout to Tutorial.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import Header from '../components/Header.jsx'
-
+import { Link } from 'react-router'
 
 function Home() {
     return(
         <>
         <div className="flex flex-col h-screen bg-amber-200">
-          <Header notLandingPage={true}/>
+          <Header notLandingPage={false}/>
           <div className="flex-1 mt-12 bg-gray-700 text-center items-center justify-center">
               <div className="flex flex-col mt-40 text-center bg-amber-50">
                     <div className="mb-10 text-8xl">RoTypeAi</div>
@@ -13,17 +13,17 @@ function Home() {
               </div>
               <div className="flex flex-col flex-wrap items-center">
                 <div className="mt-20 h-40 grid grid-cols-[275px_275px] bg-amber-900 gap-x-30">
-                      <button>
-                        <div className ="bg-pink-300 flex flex-col justify-center cursor-pointer h-full">
+                      <Link to="/Tutorial" className=" cursor-pointer">
+                        <div className="bg-pink-300 flex flex-col justify-center cursor-pointer h-full w-full">
                             <div className="text-3xl mb-8">
-                              How to Use
+                            How to Use
                             </div>
                             <div>
-                              Guided Tutorial
+                            Guided Tutorial
                             </div>
                         </div>
-                      </button>
-                      <button>
+                      </Link>
+                      <Link to="/Chatbot" className=" cursor-pointer">
                         <div className="bg-blue-300 flex flex-col justify-center cursor-pointer h-full ">
                             <div className="text-3xl mb-8">
                               Jump In
@@ -32,7 +32,7 @@ function Home() {
                               Start Typing
                             </div>
                         </div>
-                      </button>
+                      </Link>
                 </div>
               </div>
           </div>
--- src/pages/Tutorial.jsx
+++ src/pages/Tutorial.jsx
@@ -1,6 +1,18 @@
+import Header from '../components/Header.jsx'
+
 function Tutorial() {
     return(
-        <h1>This is the tutorial page</h1>
+        <>
+            <div className="flex flex-col h-screen bg-amber-200">
+                <Header notLandingPage={true}/>
+                <div className="flex-1 mt-12 bg-gray-700 text-center items-center justify-center">
+                    <div className=" border-2">
+                        <img src="https://placehold.co/600x600" alt="placeholder image"></img>
+                    </div>
+                </div>
+            </div>
+        </>
+
     );
 }
 
