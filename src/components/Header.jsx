5bd6b1d20cf4bb11cd9f28a6a1dbba971b258af7
Inderveer Johal <Inderveerjohal0@gmail.com>
2025-03-12 06:39:29 -0700

    feat: added buttons and image layout to Tutorial.jsx
    feat: changed placeholer size and positioning of elements in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ function Header(props) {
     
     const renderCheck = () => {
         if(props.notLandingPage) {
-            return  <button>
-                    <img className="pr-2 cursor-pointer"src="https://placehold.co/40x40" alt="placeholder image"></img>
+            return  <button className="mr-15 cursor-pointer">
+                    <img src="https://placehold.co/35x30" alt="placeholder image"></img>
                     </button>
         }
         else {
@@ -12,9 +12,9 @@ function Header(props) {
     };
 
     return(
-        <header className="fixed top-0 left-0 right-0 h-12">
-            <div className="flex justify-between items-center h-full bg-gray-400">
-                <div className="p-2 text-2xl">RoTypeAi</div> 
+        <header className="fixed top-0 left-0 right-0 h-15">
+            <div className="flex justify-between items-center h-full bg-gray-400 pt-2">
+                <div className="ml-10 text-2xl">RoTypeAi</div> 
                 {renderCheck()}
             </div>
         </header>
--- src/pages/Tutorial.jsx
+++ src/pages/Tutorial.jsx
@@ -3,17 +3,26 @@ import Header from '../components/Header.jsx'
 function Tutorial() {
     return(
         <>
-            
                 <Header notLandingPage={true}/>
                 <section>
-                    <div className="flex bg-gray-700 items-center justify-center h-screen">
-                        <div className=" border-2">
-                            <img src="https://placehold.co/600x600" alt="placeholder image"></img>
+                    <div className="pt-20 flex flex-col bg-gray-700 items-center justify-center h-screen">
+                        <div className="border-2">
+                            <img className="w-337.5 h-162.5 object-contain"src="https://placehold.co/1350x650" alt="placeholder image"></img>
+                        </div >
+                        <div className="mt-10 grid grid-cols-[200px_200px] grid-rows-[75px] bg-amber-900 gap-x-14">
+                            <div className="bg-pink-300 flex items-center justify-center">
+                                <div className="text-3xl">
+                                Back
+                                </div>
+                            </div>
+                            <div className="bg-blue-300 flex items-center justify-center">
+                                <div className="text-3xl">
+                                Next
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </section>
-                
-          
         </>
 
     );
