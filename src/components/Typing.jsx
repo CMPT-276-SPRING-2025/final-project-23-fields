3968570c812a0b7e15287ead7aa936d8db4184e9
kirjh <71944013+kirjh@users.noreply.github.com>
2025-03-13 01:23:39 -0700

    Add keystroke registration

--- src/components/Typing.jsx
+++ src/components/Typing.jsx
@@ -2,6 +2,8 @@ import { useEffect } from 'react'
 
 export default function Typing({ paragraph, results, updateParagraph, updateResults }) {
   let wordArray;
+  const correctKeyClasses = ['correct', 'text-white'];
+  const wrongKeyClasses = ['wrong', 'text-red-500'];
 
   // On paragraph update
   useEffect(() => {
@@ -12,31 +14,74 @@ export default function Typing({ paragraph, results, updateParagraph, updateResu
   useEffect(() => {
     const keyPress = (event) => {
       const key = event.key;
-      const expectedKey = document.querySelector('.letter.current').innerHTML;
+      const currentWord = document.querySelector('.word.current');
+      const currentLetter = document.querySelector('.letter.current');
+      const expectedKey = currentLetter ? currentLetter.innerHTML : ' ';
+      const isSpace = key == ' ';
+      const isBackspace = key.length != 1;
 
       console.log(key + " : " + expectedKey)
+      // Condition: Valid key is pressed
+      if (!isBackspace && !isSpace) {
+        // Condition: Not at the end of the word
+        if (currentLetter) {
+          addClass(currentLetter, key === expectedKey ? correctKeyClasses: wrongKeyClasses);
+          removeClass(currentLetter, 'current');
+          if (currentLetter.nextSibling) addClass(currentLetter.nextSibling, 'current');
+        // Condition: At the end of the word
+        } else {
+          const letterSpan = document.createElement("span");
+          addClass(letterSpan, wrongKeyClasses.concat(["letter"]))
+          letterSpan.innerHTML = key;
+          currentWord.appendChild(letterSpan);
+        }
+      // Condition: Space key is pressed
+      } else if (isSpace) {
+        // Condition: Space key is expected
+        if (expectedKey != ' ') {
+          const invalidatedLetters = [...document.querySelectorAll('.word.current .letter:not(correct)')];
+          invalidatedLetters.forEach(l => {
+            addClass(l, wrongKeyClasses);
+          });
+        }
+        // Move onto next word
+        removeClass(currentWord, 'current');
+        addClass(currentWord.nextSibling, 'current');
+        if (currentLetter) {
+          removeClass(currentLetter, 'current');
+        }
+        addClass(currentWord.nextSibling.firstChild, 'current');
+      }
     };
 
-    document.addEventListener('keyup', keyPress);
+    document.getElementById("typingtest").addEventListener('keyup', keyPress);
 
     return () => {
-      document.removeEventListener('keyup', keyPress);
+      document.getElementById("typingtest").removeEventListener('keyup', keyPress);
     };
   }, []);
 
   function addClass(element, name) {
-    element.classList.add(name);
+    if (Array.isArray(name)) {
+      element.classList.add(...name);
+    } else {
+      element.classList.add(name);
+    }
   }
 
   function removeClass(element, name) {
-    element.classList.remove(name);
+    if (Array.isArray(name)) {
+      element.classList.remove(...name);
+    } else {
+      element.classList.remove(name);
+    }
   }
 
   function formatWord(word) {
     const newWord = document.createElement("div");
     const letters = word.toLowerCase().split('');
 
-    newWord.className = "word inline-block m-0.5";
+    newWord.className = "word inline-block m-0.5 select-none";
 
     for (const letter of letters) {
       const letterSpan = document.createElement("span");
@@ -60,25 +105,19 @@ export default function Typing({ paragraph, results, updateParagraph, updateResu
     }
     
     addClass(document.querySelector('.word'), 'current');
-    addClass(document.querySelector('.letter'), 'current')
+    addClass(document.querySelector('.letter'), 'current');
   }
 
   return (
     <>
       <h1>This is a Typing Test</h1>
       <button onClick={()=>updateParagraph("Lorem ipsum dolor sit amet consectetur adipiscing elit")}>Text</button>
-      <div id="typingtest" className="bg-red-300">
+      <div id="typingtest" tabIndex="0" className="bg-red-300 focus:bg-blue-300">
         <div id="header">
           <div id="timer"></div>
-
         </div>
-        <div id="words">
-
-        </div>
-        <div id="cursor">
-
-        </div>
-
+        <div id="words"></div>
+        <div id="cursor"></div>
       </div>
     </>
   )
