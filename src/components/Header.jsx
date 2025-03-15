d690a067876167faba7395259337b3f70771bbd8
Inderveer Johal <Inderveerjohal0@gmail.com>
2025-03-14 17:36:31 -0700

    feat: created Menu.jsx
    feat: added menu dropdown to Menu.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
+import Menu from './Menu.jsx'
+
 function Header(props) {
     
     const renderCheck = () => {
         if(props.notLandingPage) {
-            return  <button className="mr-15 cursor-pointer">
-                    <img src="https://placehold.co/35x30" alt="placeholder image"></img>
-                    </button>
+            return  <Menu/>
         }
         else {
             return null
@@ -15,7 +15,9 @@ function Header(props) {
         <header className="fixed top-0 left-0 right-0 h-15">
             <div className="flex justify-between items-center h-full bg-gray-400 pt-2">
                 <div className="ml-10 text-2xl">RoTypeAi</div> 
-                {renderCheck()}
+                <div className="relative">
+                    {renderCheck()}
+                </div>
             </div>
         </header>
     );
