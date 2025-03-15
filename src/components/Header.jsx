20dbd6101db56379587d2271a6cb676971f0702f
Brendan Lim <brendan.james.lim@gmail.com>
2025-03-15 13:40:56 -0700

    changed colors on landing page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,8 +13,8 @@ function Header(props) {
 
     return(
         <header className="fixed top-0 left-0 right-0 h-15">
-            <div className="flex justify-between items-center h-full bg-gray-400 pt-2">
-                <div className="ml-10 text-2xl">RoTypeAi</div> 
+            <div className="flex justify-between items-center h-full bg-zinc-800 pt-2">
+                <div className="ml-10 text-2xl text-blue-600">RoTypeAi</div> 
                 {renderCheck()}
             </div>
         </header>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,13 @@ function Home() {
     return(
         <>
           <Header notLandingPage={false}/>
-          <div className="bg-gray-700 text-center h-screen">
-              <div className="flex flex-col pt-40 bg-amber-300">
-                    <div className="mb-10 text-8xl">RoTypeAi</div>
+          <div className="bg-zinc-800 text-center h-screen">
+              <div className="flex flex-col pt-40 bg-zinc-800">
+                    <div className="mb-10 text-8xl text-blue-600 .font-jost">RoTypeAi</div>
                     <div className="text-2xl">Create Personalized Typing Tests</div>
               </div>
               <div className="flex flex-col flex-wrap items-center">
-                <div className="mt-35 h-40 grid grid-cols-[275px_275px] bg-amber-900 gap-x-25">
+                <div className="mt-35 h-40 grid grid-cols-[275px_275px] bg-zinc-800 gap-x-25">
                       <Link to="/Tutorial" className=" cursor-pointer">
                         <div className="bg-pink-300 flex flex-col justify-center h-full">
                             <div className="text-3xl mb-8">
