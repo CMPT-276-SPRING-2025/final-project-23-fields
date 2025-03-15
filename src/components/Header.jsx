fcdef0e3d1fa58b6f7026d13a731d2fd80daf5f3
Brendan Lim <brendan.james.lim@gmail.com>
2025-03-15 14:13:32 -0700

    changed fonts and colors on landing page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ function Header(props) {
     return(
         <header className="fixed top-0 left-0 right-0 h-15">
             <div className="flex justify-between items-center h-full bg-zinc-800 pt-2">
-                <div className="ml-10 text-2xl text-blue-600">RoTypeAi</div> 
+                <div className="ml-10 text-2xl text-blue-600 font-jost font-bold">RoTypeAI</div> 
                 {renderCheck()}
             </div>
         </header>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,13 +7,13 @@ function Home() {
           <Header notLandingPage={false}/>
           <div className="bg-zinc-800 text-center h-screen">
               <div className="flex flex-col pt-40 bg-zinc-800">
-                    <div className="mb-10 text-8xl text-blue-600 .font-jost">RoTypeAi</div>
-                    <div className="text-2xl">Create Personalized Typing Tests</div>
+                    <div className="mb-10 text-8xl text-blue-600 font-jost font-bold">RoTypeAI</div>
+                    <div className="text-2xl text-white font-jost font-medium">Create Personalized Typing Tests</div>
               </div>
               <div className="flex flex-col flex-wrap items-center">
                 <div className="mt-35 h-40 grid grid-cols-[275px_275px] bg-zinc-800 gap-x-25">
                       <Link to="/Tutorial" className=" cursor-pointer">
-                        <div className="bg-pink-300 flex flex-col justify-center h-full">
+                        <div className="shadow-md rounded-md bg-gray-700 text-white font-jost flex flex-col justify-center h-full">
                             <div className="text-3xl mb-8">
                             How to Use
                             </div>
@@ -23,7 +23,7 @@ function Home() {
                         </div>
                       </Link>
                       <Link to="/Chatbot" className=" cursor-pointer">
-                        <div className="bg-blue-300 flex flex-col justify-center h-full">
+                        <div className="rounded-md shadow-md text-white font-jost bg-gray-700 flex flex-col justify-center h-full">
                             <div className="text-3xl mb-8">
                               Jump In
                             </div>
