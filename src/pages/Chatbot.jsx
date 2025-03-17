f5129261be16549ea28ef7067d6cf0a302a8509b
kirjh <71944013+kirjh@users.noreply.github.com>
2025-03-16 21:43:32 -0700

    Add timer to typing test

--- src/pages/Chatbot.jsx
+++ src/pages/Chatbot.jsx
@@ -7,6 +7,10 @@ function Chatbot() {
     const [paragraph, setParagraph] = useState({
         text: null
     });
+
+    // (int) time limit of typing test in milliseconds (default is 30000)
+    const [typingTime, setTypingTime] = useState(30000);
+
     // State for storing user input from text box
     const [userInput, setUserInput] = useState('');
 
@@ -89,10 +93,11 @@ function Chatbot() {
                 <button type="submit">Send</button>
             </form>
             {paragraph.text ? (
-                <Typing paragraph={paragraph} updateParagraph={updateParagraph} results={results} updateResults={updateResults}/>
+                <p>e</p>//<Typing paragraph={paragraph} updateParagraph={updateParagraph} results={results} updateResults={updateResults}/>
             ) : (
                 <p>Bot: {botResponse}</p>
             )}
+            <Typing paragraph={paragraph} updateParagraph={updateParagraph} results={results} updateResults={updateResults} typingTime={typingTime} setTypingTime={setTypingTime}/>
         </div>
     );
 }
