2f6713aaad1f0ca0e1dfe9bc9af3b35016def8bd
Brendan Lim <brendan.james.lim@gmail.com>
2025-04-04 13:45:10 -0700

    styled header links

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -14,10 +14,10 @@ function Menu() {
         <>
             <ul className="flex absolute right-10 space-x-5 -bottom-3">
                 <Link to="/Tutorial" className=" cursor-pointer">
-                    <li className="cursor-pointer font-jost text-white">Tutorial</li>
+                    <li className="text-xl cursor-pointer hover:underline font-jost text-white">Tutorial</li>
                 </Link>
                 <Link to="/ChatBot" className=" cursor-pointer">
-                    <li className="cursor-pointer font-jost text-white">ChatBot</li>
+                    <li className="text-xl cursor-pointer hover:underline font-jost text-white">ChatBot</li>
                 </Link>
             </ul>
         </>
