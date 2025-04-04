a0bbe57230240cac402cb5d69e76d652c1a7ab54
Brendan Lim <brendan.james.lim@gmail.com>
2025-04-04 13:42:53 -0700

    removed menu icon

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -12,9 +12,7 @@ function Menu() {
 
     return(
         <>
-        {expanded && 
-        <>
-            <ul className="flex absolute right-30 space-x-5 top-0.5">
+            <ul className="flex absolute right-10 space-x-5 -bottom-3">
                 <Link to="/Tutorial" className=" cursor-pointer">
                     <li className="cursor-pointer font-jost text-white">Tutorial</li>
                 </Link>
@@ -22,10 +20,6 @@ function Menu() {
                     <li className="cursor-pointer font-jost text-white">ChatBot</li>
                 </Link>
             </ul>
-        </>}
-        <button className="mr-15 cursor-pointer" onClick={Expand}>
-            <img src={"./public/icons/menu.svg"} alt="menu icon" className="mt-px"></img>
-        </button>
         </>
     );
 }
