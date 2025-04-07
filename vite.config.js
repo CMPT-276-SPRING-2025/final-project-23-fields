e9cc25b7944240fb9728ff26bf3c26b55816c4e1
Brendan Lim <brendan.james.lim@gmail.com>
2025-04-07 09:31:28 -0700

    created tests for mediawiki

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./tests/setup.js'],
+    exclude: [
+      '**/node_modules/**',
+      '**/selenium-tests/**'
+    ]
+  }
 })
